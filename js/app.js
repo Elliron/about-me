45e8db766632534df41b3d2dccead0a4721c4679
Glenn <Elliron.Clark@gmail.com>
2020-12-03 14:19:38 -0800

    cleaned up comments and names

--- js/app.js
+++ js/app.js
@@ -3,49 +3,60 @@
 alert('Welcome to the greatest page you will ever see!!!');
 var userName = prompt('Choose your name');
 // console.log(userName);
+var totalAnswer = 0;
+
 alert('Hiya ' + userName + ' I hope you studied.');
 
-var game = prompt('Do you want to play a game? yes or no').toLowerCase();
-// console.log(game);
-if (game === 'yes' || game === 'y') {
-  // console.log('Very well, ' + userName + ' answered ' + game + ' to playing a game');
+// question 1
+var questOne = prompt('Do you want to play a game? yes or no').toLowerCase();
+// console.log(questOne);
+if (questOne === 'yes' || questOne === 'y') {
+  // console.log('Very well, ' + userName + ' answered ' + questOne + ' to playing a game');
+  totalAnswer++;
   alert('Let\'s have some fun!');
 } else {
   alert('Too bad, Haha!');
 }
 
-var quest1 = prompt('Do I live in Washington? yes or no').toLowerCase();
-// console.log(quest1);
-if (quest1 === 'yes' || quest1 === 'y') {
-  // console.log(userName + ' answered ' + quest1 + ' to question 1');
+//question 2
+var questTwo = prompt('Do I live in Washington? yes or no').toLowerCase();
+// console.log(questTwo);
+if (questTwo === 'yes' || questTwo === 'y') {
+  // console.log(userName + ' answered ' + questTwo + ' to question 1');
+  totalAnswer++;
   alert('That was an easy one.');
 } else {
   alert('Wrong! Haha');
 }
 
-var quest2 = prompt('Did I go to Western Washington University? yes or no').toLowerCase();
-// console.log(quest2);
-if (quest2 === 'no' || quest2 === 'n') {
-  // console.log(userName + ' answered ' + quest2 + ' question 2');
+//question 3
+var questThree = prompt('Did I go to Western Washington University? yes or no').toLowerCase();
+// console.log(questThree);
+if (questThree === 'no' || questThree === 'n') {
+  // console.log(userName + ' answered ' + questThree + ' question 2');
+  totalAnswer++;
   alert('Good Job.');
 } else {
   alert('Better luck next time');
 }
 
-// Question Four y/n
-var quest3 = prompt('Do I have kitties? yes or no').toLowerCase();
-// console.log(quest3);
-if (quest3 === 'no' || quest3 === 'n') {
+// question 4 y/n
+var questFour = prompt('Do I have kitties? yes or no').toLowerCase();
+// console.log(questFour);
+if (questFour3 === 'no' || questFour === 'n') {
   // console.log(userName + ' answered ' + quest3 + ' to question 3');
+  totalAnswer++;
   alert('Sadly I dont, but hopefully soon.');
 } else {
   alert('Just rub salt in the wound, I will have them soon though!');
 }
 
-var quest4 = prompt('Is Lauren the Best? yes or no, but mostly yes');
-// console.log(quest4);
-if (quest4 === 'yes' || quest4 === 'y') {
-  // console.log(userName + ' answered ' + quest4 + ' to question 4');
+//question 5
+var questFive = prompt('Is Lauren the Best? yes or no, but mostly yes');
+// console.log(questFive);
+if (questFive === 'yes' || questFive === 'y') {
+  // console.log(userName + ' answered ' + questFive + ' to question 4');
+  totalAnswer++;
   alert('You have good taste!');
 } else {
   alert('...');
@@ -53,15 +64,16 @@ if (quest4 === 'yes' || quest4 === 'y') {
 
 // question 6
 var attempts = 4;
-var quest6Ans = 8;
+var questSixAns = 8;
 for (var i = 0; i < attempts; i++) {
   var quest6 = parseInt(prompt('Guess a number between 1 and 10.'));
-  if (quest6 === quest6Ans) {
+  if (quest6 === questSixAns) {
+    totalAnswer++;
     alert('Good Job');
     break;
-  } else if (quest6 > quest6Ans && i !== 3) {
+  } else if (quest6 > questSixAns && i !== 3) {
     alert('Too High');
-  } else if (quest6 < quest6Ans && i !== 3) {
+  } else if (quest6 < questSixAns && i !== 3) {
     alert('Too Low');
   } else {
     alert('Game Over');
@@ -71,22 +83,26 @@ for (var i = 0; i < attempts; i++) {
 // question 7
 
 var food = ['sushi', 'pizza', 'drunken noodle', 'ramen'];
+var questSevenAttempts = 6
+var answeredCorrectly = false;
 console.log(food);
-loop1: for (var i = 0; i < 6; i++) {
-  var quest7 = prompt('What are my favorite foods?').toLowerCase();
+While (!answeredCorrectly && questSevenAttempts > 0); {
+  var questSeven = prompt('What are my favorite foods?').toLowerCase();
   // console.log('food[j]', food[j]);
   // console.log('quest7', quest7);
   for (var j = 0; j < food.length; j++) {
-    if (quest7 === food[j]) {
+    if (questSeven === food[j]) {
+      totalAnswer++;
       alert('Good Job');
-      break loop1;
+      if !answeredCorrectly = false;
+      break;
     }
-    else if (quest7 !== food[j]) {
-      alert('sorry, try again');
+    else if (questSeven !== food[j]) {
+      alert('sorry, try again')
       break;
     }
   }
 }
-alert(`The correct answers are ${food}`);
+alert(`The correct answers are ${food}, you answered ${totalAnswer} correct.`);
 
 alert(`Thank's ${userName}, for taking an interest in my journey and playing my game.`);
